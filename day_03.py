086d153c978918cd37c0d097fe2c40c5c58e80c8
riaz <riazjahangir@gmail.com>
2023-12-09 00:49:44 -0600

    day 3 part 2

--- day_03.py
+++ day_03.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 from utils import get_input_lines
 
 
@@ -10,6 +11,9 @@ class Number:
 
     def is_part_number(self, schematic):
         return is_number_adjacent_to_symbol(self, schematic)
+    
+    def get_adjacent_gears(self, schematic):
+        self.adjacent_gears = find_adjacent_gears(self, schematic)        
 
 
 def find_numbers(schematic):
@@ -41,7 +45,39 @@ def is_number_adjacent_to_symbol(num, schematic):
     return False
 
 
+def find_adjacent_gears(num, schematic):
+    max_row = len(schematic) - 1
+
+    rows_to_check = (
+        [0, 1] if num.row == 0 else 
+        [max_row - 1, max_row] if num.row == max_row else
+        [num.row - 1, num.row, num.row + 1]
+    )
+    cols_to_check = range(max(0, num.col-1), min(num.col+len(str(num.value))+1, len(schematic[0])))
+
+    gears = []
+    for row in rows_to_check:
+        for col in cols_to_check:
+            c = schematic[row][col]
+            if c == '*':
+                gears.append((row, col))
+    return gears
+
+
 if __name__ == '__main__':
     schematic = get_input_lines(day=3)
     numbers = find_numbers(schematic)
     print('Part 1:', sum(num.value for num in numbers if num.is_part_number(schematic)))
+
+    gear_parts = defaultdict(list)
+    for number in numbers:
+        number.get_adjacent_gears(schematic)
+        for gear in number.adjacent_gears:
+            gear_parts[gear].append(number.value)
+    
+    gear_ratio_sum = 0
+    for parts in gear_parts.values():
+        if len(parts) == 2:
+            gear_ratio = parts[0] * parts[1]
+            gear_ratio_sum += gear_ratio
+    print('Part 2:', gear_ratio_sum)
