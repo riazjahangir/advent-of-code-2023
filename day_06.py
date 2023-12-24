26f17f89de1565dcbbc0d5a1803acbb2386c51d8
riaz <riazjahangir@gmail.com>
2023-12-23 23:48:22 -0600

    day 6 part 2

--- day_06.py
+++ day_06.py
@@ -17,22 +17,16 @@ def part_1(races):
     num_ways_to_win = [len(get_ways_to_win(race)) for race in races]
     return functools.reduce(lambda a, b: a * b, num_ways_to_win)
 
+
+def part_2(race):
+    return len(get_ways_to_win(race))
             
 
 
 if __name__ == '__main__':
-    races = [
-        (40, 233),
-        (82, 1011),
-        (84, 1110),
-        (92, 1487)
-    ]
-    
-    # races = [
-    #     (7, 9),
-    #     (15, 40),
-    #     (30, 200)
-    # ]
+    race = (40828492, 233101111101487)
 
-    print('Part 1:', part_1(races))
+    # race = (71530, 940200)
+    
+    print('Part 2:', part_2(race))
     
